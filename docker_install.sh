30db51504683002358d4812e6369f673958105fb
local man <localsnet@gmail.com>
2017-09-08 10:57:14 +0300

    initial docker install script

---
+++ docker_install.sh
@@ -0,0 +1,5 @@
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.16.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
+
