80e4732e0f886f862b24c187e0f38f3c5e9a7f24
local <localsnet@gmail.com>
2017-09-10 19:28:28 +0300

    fix2 script

--- docker_install.sh
+++ docker_install.sh
@@ -4,7 +4,7 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get install docker-ce
+sudo apt-get update && sudo apt-get install docker-ce
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
@@ -12,5 +12,3 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/docker/compose/1.16.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 sudo docker run hello-world
 sudo docker-compose -v
-
-
