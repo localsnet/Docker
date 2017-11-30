11512894d1044a9180ba68db73d9ba12557ce150
Local Devops <localsnet@gmail.com>
2017-11-30 16:18:10 +0300

    script edited

--- oracle_images/OracleWebLogic/weblogic_dockerize.sh
+++ oracle_images/OracleWebLogic/weblogic_dockerize.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 #Building a Docker Image for WebLogic 
-ORCL=~/repos_git/oracle_images
+ORCL=~/repos_git/Dockerfile_s/oracle_images
 DIST=~/repos_git/dist_oracle
 #1.Git clone the Oracle Docker project repository
-git clone https://github.com/oracle/docker-images.git
+git clone https://github.com/localsnet/Dockerfile_s.git
 
-mv docker-images oracle_images && cd oracle_images
 
 #2.Download required JRE file
 
 cd $ORCL/OracleJava/java-8
 
 #Copy/Download work jre file here
-
+cp $DIST /$ORCL/OracleJava/java-8
 # New version  JRE didn't work, got error "need JDK"
 #wget -c http://javadl.oracle.com/webapps/download/AutoDL?BundleId=227542_e758a0de34e24606bca991d704f6dcbf && mv AutoDL* jre-8u151-linux-x64.tar.gz
-wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
+#wget --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
 
 #Edit Dockerfile for JRE version
 sed -i 's/server-jre-8u\*-linux-x64.tar.gz/jdk-8u151-linux-x64.tar.gz/g' Dockerfile
@@ -24,10 +23,11 @@ sed -i 's/server-jre-8u\*-linux-x64.tar.gz/jdk-8u151-linux-x64.tar.gz/g' Dockerf
 ./build.sh
 
 #4.Build the second image adding WebLogic 12.1.3 generic
+#Move/Download file fmw_12.1.3.0.0_wls.jar here
+cp $DIST/fmw_12.1.3.0.0_wls.jar $ORCL/OracleWebLogic/dockerfiles/12.1.3
+
 cd $ORCL/OracleWebLogic/dockerfiles/
 
-#Move/Download file fmw_12.1.3.0.0_wls.jar here
-mv $DIST/fmw_12.1.3.0.0_wls.jar .
 ./buildDockerImage.sh -v 12.1.3 -g -c
 
 #5. Extend the Oracle WebLogic image by creating a sample empty domain
@@ -46,4 +46,3 @@ sleep 5
 #Node2 container #3
 
 docker run -d --name managed_srv_2 --link wlsadmin:wlsadmin -p 7004:7004 -e MS_PORT=7004  1213-domain createServer.sh
-sleep 5
